188ed03faebfbddbabff762b32faad7d172392c3
rejaques <renanjaques@outlook.com>
2020-03-19 12:05:09 -0300

    Mudando a cor das peças

--- CHESS/Program.cs
+++ CHESS/Program.cs
@@ -13,7 +13,7 @@ namespace CHESS
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(Cor.Branca, tab), new Posicao(1, 3));
 
                 Tela.ImprimirTabuleiro(tab);
             }
--- CHESS/Tela.cs
+++ CHESS/Tela.cs
@@ -9,14 +9,34 @@ namespace CHESS
         {
             for(int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + "  ");
                 for(int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
                         Console.Write("- ");
                     else
-                        Console.Write(tab.peca(i, j) + " ");
+                    {
+                        ImprimirPeca(tab.peca(i, j));
+                    }
+                        
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("   a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca p)
+        {
+            if (p.Cor == Cor.Branca)
+            {
+                Console.Write(p + " ");
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(p + " ");
+                Console.ForegroundColor = aux;
             }
         }
     }
