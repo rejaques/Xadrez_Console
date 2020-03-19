0b99e36d579e7dd04a5e8d7e4e45ec1a3b3de64c
rejaques <renanjaques@outlook.com>
2020-03-19 10:58:21 -0300

    Adicionadas as classes: Cor, Tabuleiro e Posicao

--- CHESS/Program.cs
+++ CHESS/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace CHESS
 {
@@ -7,9 +7,8 @@ namespace CHESS
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine("Posição: " + P);
         }
     }
 }
---
+++ CHESS/Tabuleiro/Cor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    enum Cor : int
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha
+    }
+}
